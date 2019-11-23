d13c718645e29a658104cf59b4b5036ac0c09ca1
phil lee <lee.phil@outlook.com>
2019-11-23 14:44:03 -0500

    updated bugs around infinite loop, due to not converting obj to json

--- comment-api/src/main/java/com/ga/commentapi/commentapi/service/CommentService.java
+++ comment-api/src/main/java/com/ga/commentapi/commentapi/service/CommentService.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 public interface CommentService {
     public Comment createComment(Long postId, Comment comment, String id, String username);
     public HttpStatus deleteComment(long commentId);
-    public Iterable<Comment> deleteCommentByPostId(String message);
+    public void deleteCommentByPostId(String message);
     public Iterable<Comment> getCommentsByPostId(long postId);
     public Iterable<Comment> getCommentsByUserId(String username, Long userId);
     public Iterable<Comment> getCommentsByUsername(String username);
