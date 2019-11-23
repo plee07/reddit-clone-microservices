a93b11c6171b77371ba6aa39982d958dbea06ef6
phil lee <lee.phil@outlook.com>
2019-11-23 17:22:55 -0500

    working interservice with rabbitmq

--- comment-api/src/main/java/com/ga/commentapi/commentapi/service/CommentService.java
+++ comment-api/src/main/java/com/ga/commentapi/commentapi/service/CommentService.java
@@ -2,7 +2,6 @@ package com.ga.commentapi.commentapi.service;
 
 import com.ga.commentapi.commentapi.model.Comment;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 
 
 public interface CommentService {
