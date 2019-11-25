de76e884c8705722b55aa6161182dccb6c6a7b20
ToddSing <kty5876@aol.com>
2019-11-25 15:06:13 -0500

    added swagger

--- comment-api/src/main/java/com/ga/commentapi/commentapi/repository/CommentRepository.java
+++ comment-api/src/main/java/com/ga/commentapi/commentapi/repository/CommentRepository.java
@@ -1,20 +1,28 @@
 package com.ga.commentapi.commentapi.repository;
 
 import com.ga.commentapi.commentapi.model.commentModel;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-@Repository
+@RepositoryRestResource
 public interface CommentRepository extends CrudRepository<commentModel, Long> {
 
-    public Iterable<commentModel> findCommentByPostId(Long postId);
-    public Iterable<commentModel> findCommentByUserId(Long userId);
-    public Iterable<commentModel> findCommentsByUsername(String username);
+    @RestResource(path = "comment")
+    @ApiOperation("Find comments by the postId")
+    public Iterable<commentModel> findCommentByPostId(@Param("postId") Long postId);
+
+    @ApiOperation("Find comments by User's username")
+    public Iterable<commentModel> findCommentsByUsername(@Param("username") String username);
 
     @Transactional
     @Modifying
-    public Iterable<commentModel> deleteCommentsByPostId(Long postId);
+    @ApiOperation("Delete comments from Post. Interservice call")
+    public Iterable<commentModel> deleteCommentsByPostId(@Param("postId") Long postId);
 
 }
