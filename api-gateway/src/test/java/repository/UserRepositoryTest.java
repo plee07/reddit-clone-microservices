fd3c5a1f3b1b517ae19d8cb0f726efb8c519fcef
phil lee <lee.phil@outlook.com>
2019-12-01 20:59:08 -0500

    api-gateway, userrep test

---
+++ api-gateway/src/test/java/repository/UserRepositoryTest.java
@@ -0,0 +1,62 @@
+package repository;
+
+import com.example.apigateway.model.User;
+import com.example.apigateway.repository.UserRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UserRepositoryTest {
+
+    @InjectMocks
+    private UserRepository userRepository;
+
+    @Mock
+    private JdbcTemplate jdbc;
+
+    @Mock
+    private ResultSet rs;
+
+    @Captor
+    private ArgumentCaptor<RowMapper<User>> rowMapperCaptor;
+
+    private User user;
+    @Before
+    public void test()throws SQLException {
+        user = new User(1L, "batman", "pass123");
+        when(rs.getLong(any())).thenReturn(user.getUserId());
+        when(rs.getString(any())).thenReturn(user.getUsername(), user.getPassword());
+    }
+    @Test
+    public void getUserByUserName_SUCCESS() throws SQLException {
+        User testUser = userRepository.getUserByUsername("batman");
+        verify(jdbc).queryForObject(anyString(), any(), rowMapperCaptor.capture());
+        RowMapper<User> rm = rowMapperCaptor.getValue();
+        User test = rm.mapRow(rs, 1);
+        assertEquals(test.getUsername(), user.getUsername());
+        assertEquals(test.getPassword(), user.getPassword());
+        assertEquals(test.getUserId(), user.getUserId());
+    }
+}
